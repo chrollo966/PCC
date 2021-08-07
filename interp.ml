119a84dd690e7541c66a6154618167c11b01eb9a
chrollo966 <rhinocerossutra@gmail.com>
2021-08-07 22:33:03 +0900

    variants and some functions are defined

--- interp.ml
+++ interp.ml
@@ -1,11 +1,11 @@
 type id = string
-type op = Plus | Minus | Times | Div ;;
-type stm
-  = Stmts of stm * stm
-  | Assign of id * exp
-  | Print of exp
-and exp
-  = ID of id
+
+type op = Plus | Minus | Times | Div
+
+type stm = Stmts of stm * stm | Assign of id * exp | Print of exp
+
+and exp =
+  | ID of id
   | Num of int
   | Plus of exp * exp
   | Minus of exp * exp
@@ -13,8 +13,19 @@ and exp
   | Div of exp * exp
 
 exception No_such_symbol
-let e0 = fun _ -> raise No_such_symbol
-let update var vl env =
-  fun v -> if v = var
-    then vl
-    else env v
+
+let e0 _ = raise No_such_symbol
+
+let update var vl env v = if v = var then vl else env v
+
+let rec trans_stmt ast env =
+  match ast with
+  | Stmts (s1, s2) ->
+      let env' = trans_stmt sq env in
+      trans_stms s2 env
+  | Assign (var, e) ->
+      let vl = trans_exp e env in
+      update var vl env
+  | Print e ->
+      let vl = trans_exp e env in
+      (print_int vl; print_string "\n"; env)
